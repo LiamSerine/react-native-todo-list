4ba99f0a198c09541571f0153fe2bb9eb97351fa
Liam <liam.serine@diiage.org>
2021-04-01 13:11:44 +0200

    feat(new task): add new task section
    feat(new task): add new task section

--- todoList/App.js
+++ todoList/App.js
@@ -1,46 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{ useState, useEffect } from 'react';
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Task from './components/Task';
+const data = [
+  {
+    label: '🏋 Work out 30 minutes'
+  },
+  {
+    label: '🏫 Ace the react native test'
+  },
+  {
+    label: ' 🍸 Chill out'
+  },
+]
 
 export default function App() {
+  const [TASKS, setTASKS] = useState(data);
+  const [newTask, setNewTask] = useState("");
+  function addTask()
+  {
+    var donnee = TASKS;
+    donnee.push({label: newTask})
+    setTASKS(donnee);
+    setNewTask("");
+    
+  }
+
   return (
     <View style={styles.container} key="">
       {/* Todo today section */}      
       <View style={styles.tasksSection}>
         <Text style={styles.Titre}>Todo today</Text>
         {
-          data.map(d => {
+          TASKS.map(d => {
             return(
               <Task initialParams={{d}} ></Task>
             )
           })
         }
       </View>
+      <View style={{flexDirection: "row", justifyContent: "center", marginTop: 20, borderRadius: 10}}>
+        <KeyboardAvoidingView style={{flexDirection: "row"}}>
+          <TextInput 
+          onChangeText={(text) => {
+            setNewTask(text);
+          }}
+          value={newTask} style={{backgroundColor: "white", borderRadius: 10, textAlign: "center"}} placeholder="Add a task">
+          </TextInput>
+          <TouchableOpacity style={{backgroundColor: "white", height: 50, width: 50, borderRadius: 20, justifyContent: "center", alignItems: "center", marginLeft: 20}} 
+            onPress={addTask}>          
+            <Text>+</Text>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+      </View>
     </View>
   );
 }
 
-const data = [
-  {
-    id: 1,
-    thumbnail: '',
-    label: '🏋 Work out 30 minutes',
-    isCompleted: false
-  },
-  {
-    id: 2,
-    thumbnail: '',
-    label: '🏫 Ace the react native test',
-    isCompleted: false
-  },
-  {
-    id: 3,
-    thumbnail: '',
-    label: ' 🍸 Chill out',
-    isCompleted: false
-  },
- ]
+
 
 const styles = StyleSheet.create({
   container: {
